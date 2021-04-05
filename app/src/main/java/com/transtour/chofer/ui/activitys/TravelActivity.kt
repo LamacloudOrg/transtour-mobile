3954f2934a3b4483a76ed0f5e17e55d11c3fe90d
pabloOcanto <pomalianni@gmail.com>
2021-04-05 18:23:03 -0300

    - correccion dependencia para andriod 7 - activity para inicar la app que levante y firme el pdf

--- app/src/main/java/com/transtour/chofer/ui/activitys/TravelActivity.kt
+++ app/src/main/java/com/transtour/chofer/ui/activitys/TravelActivity.kt
@@ -1,9 +1,15 @@
 package com.transtour.chofer.ui.activitys
 
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.view.View
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.transtour.chofer.R
 
 class TravelActivity : AppCompatActivity() {
@@ -17,4 +23,24 @@ class TravelActivity : AppCompatActivity() {
         val textViewChofer:TextView = findViewById(R.id.textViewChofer)
         textViewChofer.text = user
     }
+
+    fun signDocument(view: View) {
+
+        val path = Environment.getExternalStorageState()
+        val fileName ="Payment Voucher Template PDF.pdf"
+        val id_file  = 1
+
+        val intent = getPackageManager().getLaunchIntentForPackage("com.xodo.pdf.reader");
+        if (intent != null) {
+               intent.apply {
+                   addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                   addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                   putExtra(DocumentsContract.EXTRA_INITIAL_URI,path+"/"+fileName)
+
+               }
+             startActivity(intent)
+        }
+    }
+
+
 }
