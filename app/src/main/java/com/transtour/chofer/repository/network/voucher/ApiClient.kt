a6cf5b255f6836b30f026a8aabfbfd40f13c9c21
pabloOcanto <pomalianni@gmail.com>
2021-07-28 21:11:42 -0300

    fix en endpoint

--- app/src/main/java/com/transtour/chofer/repository/network/voucher/ApiClient.kt
+++ app/src/main/java/com/transtour/chofer/repository/network/voucher/ApiClient.kt
@@ -1,14 +1,11 @@
 package com.transtour.chofer.repository.network.voucher
 
 import com.transtour.chofer.model.Signature
-import com.transtour.chofer.model.UserRegisterNotification
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.Headers
 import retrofit2.http.POST
-import retrofit2.http.Part
 
 interface ApiClient {
-    @POST("api/service-voucher/v1/voucher/signature")
-    suspend fun signVoucher(@Body singture:Signature): Response<String>
+    @POST("api/service-voucher/v1/voucher/saveSignature")
+    suspend fun signVoucher(@Body singture: Signature): Response<String>
 }
